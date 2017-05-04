465ccbd434e3e468a7ae80decbceb5544e513eec
psyfrett <psyfrett@users.noreply.github.com>
2017-05-03 22:00:11 -0500

    created a variable and referenced it in a print function

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('Async Comp' * 5)
+# look at me go! comments and all
+# I should comment this when I understand its voodoo
+
+best_name_eva = 'Async Comp' + '!' * 3 
+
+print("%s" % best_name_eva)
